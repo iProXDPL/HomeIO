906fa8d31b9825d7b2dbe2abb0cfc8b5d26f300a
Krzysztof W <45880989+iProXDPL@users.noreply.github.com>
2023-01-25 12:20:36 +0100

    Update firebase.js

--- Frontend/src/utilities/firebase.js
+++ Frontend/src/utilities/firebase.js
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCKsYWcl14vwu5zoy3upBWdUuksN0FnXc4",
-    authDomain: "homeio-test.firebaseapp.com",
-    databaseURL: "https://homeio-test-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "homeio-test",
-    storageBucket: "homeio-test.appspot.com",
-    messagingSenderId: "78266375154",
-    appId: "1:78266375154:web:fa51ecee8a6ea573e7296b",
-    measurementId: "G-HHHFKHQ2QQ"
+    apiKey: " ",
+    authDomain: " ",
+    databaseURL: " ",
+    projectId: " ",
+    storageBucket: " ",
+    messagingSenderId: " ",
+    appId: " ",
+    measurementId: " "
 };
 
 
